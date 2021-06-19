d67d849a8e2599cfede839e7239db0dc7bdf8234
kyungpyo-kim <kyungpyo94@gmail.com>
2021-06-19 05:24:22 +0000

    init commit

---
+++ data/split.py
@@ -0,0 +1,43 @@
+
+import os
+import glob
+
+file_num = []
+for dir in ['training', 'validation']:
+    i = 0
+    for _ in glob.iglob(dir + '/**/**', recursive=True):
+      i += 1
+
+    print("number of {} files: {}".format(dir, i))
+    file_num.append(i)
+
+assert(file_num[0] == file_num[1])
+
+lines_train = [line.rstrip('\n') for line in open('train.txt')]
+lines_val = [line.rstrip('\n') for line in open('val.txt')]
+
+for i in lines_train:
+  os.remove('training/image_2/'+i+'.png')
+  os.remove('training/label_2/'+i+'.txt')
+  os.remove('training/velodyne/'+i+'.bin')
+
+for i in lines_val:
+  os.remove('validation/image_2/'+i+'.png')
+  os.remove('validation/label_2/'+i+'.txt')
+  os.remove('validation/velodyne/'+i+'.bin')
+
+for dir in ['training', 'validation']:
+    i = 0
+    for _ in glob.iglob(dir + '/image_2/**/**', recursive=True):
+      i += 1
+    print("number of {} image files: {}".format(dir, i))
+
+    i = 0
+    for _ in glob.iglob(dir + '/label_2/**/**', recursive=True):
+      i += 1
+    print("number of {} lable files: {}".format(dir, i))
+    
+    i = 0
+    for _ in glob.iglob(dir + '/velodyne/**/**', recursive=True):
+      i += 1
+    print("number of {} velodyne files: {}".format(dir, i))
